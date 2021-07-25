fe486e5226108a271811cdac9933cc7c3311c30b
Vinícius Pedro da Silveira <viniciusps_ph@hotmail.com>
2021-07-25 03:27:00 -0300

    Criando Template para o projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TemplateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
